7ec3ded25ee5079827593903fcb4211b4f6385c5
Mário César <mariocesarmoliveirafilho@gmail.com>
2024-12-02 22:32:40 -0300

    Projeto Criado

---
+++ src/main/java/com/br/trabalhouegn2/Trabalhouegn2Application.java
@@ -0,0 +1,13 @@
+package com.br.trabalhouegn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhouegn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhouegn2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/trabalhouegn2/Trabalhouegn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.trabalhouegn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalhouegn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
