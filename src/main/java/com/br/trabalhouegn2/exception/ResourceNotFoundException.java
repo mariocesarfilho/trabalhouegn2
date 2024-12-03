355a01287e15b1de0ff968af1b5baf396ffb5cd2
Mário César <mariocesarmoliveirafilho@gmail.com>
2024-12-02 22:42:59 -0300

    Classe repository criada

---
+++ src/main/java/com/br/trabalhouegn2/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.br.trabalhouegn2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serivalVersionUID = 1L;
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
