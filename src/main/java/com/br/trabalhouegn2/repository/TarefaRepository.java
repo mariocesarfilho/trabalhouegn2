b61a1d12ed79b56bfe6d4c6f15ae110a2c993383
Mário César <mariocesarmoliveirafilho@gmail.com>
2024-12-02 22:47:07 -0300

    Repository criada

---
+++ src/main/java/com/br/trabalhouegn2/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package com.br.trabalhouegn2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.trabalhouegn2.model.Tarefa;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Integer>{
+
+}
